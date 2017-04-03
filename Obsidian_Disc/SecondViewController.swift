41db1d0b7f99852f43d21c5e8d7c4480afb78c06
Rulon Wood <rulonsdangerwoodiv@gmail.com>
2017-04-03 15:14:57 -0600

    Initial Commit
    This is the beginning of the end…. of the semester.

---
+++ Obsidian_Disc/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Obsidian_Disc
+//
+//  Created by Rulon Wood on 4/3/17.
+//  Copyright © 2017 Utah State University. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
