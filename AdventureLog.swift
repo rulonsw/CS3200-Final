ebbe21f94828e59e0acc489a7ccd0d9e8293528e
Rulon Wood <rulonsdangerwoodiv@gmail.com>
2017-04-03 18:39:14 -0600

    I've done a bad thing
    Creating the custom cell for my adventure log class has angered the
    Swift gods. Calling it for today.

---
+++ AdventureLog.swift
@@ -0,0 +1,24 @@
+//
+//  AdventureLog.swift
+//  Obsidian_Disc
+//
+//  Created by Rulon Wood on 4/3/17.
+//  Copyright © 2017 Utah State University. All rights reserved.
+//
+
+import UIKit
+
+class AdventureLog: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
