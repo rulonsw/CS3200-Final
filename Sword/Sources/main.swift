2ac26770a5b54d9ea40f296520dc1c29471d54d8
Rulon Wood <rulonw@ifit.com>
2017-05-31 15:41:40 -0600

    delete bot token
    Whoops, someone found it :(

--- Sword/Sources/main.swift
+++ Sword/Sources/main.swift
@@ -1,7 +1,6 @@
 import Sword
 
     
-let bot = Sword(token: "MzA2MDY5MjExNTAxODIxOTUz.C-A_tQ.8tBeOVu46mlP9Sv72eI4gP14uyA")
 
 bot.on(.ready) { _ in
   bot.editStatus(to:Presence(status: .online, playing:"With Swift, God Dammit"))
